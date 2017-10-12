3f3dc29ef6f29723ddbb78acf8c9551551b69b9e
KleinYuan <kaiwen.yuan1992@gmail.com>
2017-10-12 14:07:40 -0700

    Futher clean

--- services/weights_load_services.py
+++ services/weights_load_services.py
@@ -3,22 +3,23 @@ import numpy as np
 
 
 def load_alexnet_pre_trained_weights(weight_path, skip_layers, session):
-    weights_dict = np.load(weight_path, encoding='bytes').item()
-    # Loop over all layer names stored in the weights dict
-    for op_name in weights_dict:
-        # Check if the layer is one of the layers that should be reinitialized
-        if op_name not in skip_layers:
+    with session.graph.as_default():
+        weights_dict = np.load(weight_path, encoding='bytes').item()
+        # Loop over all layer names stored in the weights dict
+        for op_name in weights_dict:
+            # Check if the layer is one of the layers that should be reinitialized
+            if op_name not in skip_layers:
 
-            with tf.variable_scope(op_name, reuse=True):
+                with tf.variable_scope(op_name, reuse=True):
 
-                # Loop over list of weights/biases and assign them to their corresponding tf variable
-                for data in weights_dict[op_name]:
-                    # Biases
-                    if len(data.shape) == 1:
+                    # Loop over list of weights/biases and assign them to their corresponding tf variable
+                    for data in weights_dict[op_name]:
+                        # Biases
+                        if len(data.shape) == 1:
 
-                        var = tf.get_variable('biases', trainable=False)
-                        session.run(var.assign(data))
-                    # Weights
-                    else:
-                        var = tf.get_variable('weights', trainable=False)
-                        session.run(var.assign(data))
+                            var = tf.get_variable('biases', trainable=False)
+                            session.run(var.assign(data))
+                        # Weights
+                        else:
+                            var = tf.get_variable('weights', trainable=False)
+                            session.run(var.assign(data))
